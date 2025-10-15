c8c4310fbfeea1e4db37fa702b943e2744d20b1d
Winn <whitemingcyj@outlook.com>
2025-10-16 04:09:45 +0800

    'Sprint1'

--- front_end/src/services/api.ts
+++ front_end/src/services/api.ts
@@ -158,7 +158,18 @@ class ApiService {
       const uid: string = user.studentId ?? user.id ?? user.student_id ?? String(studentId);
       localStorage.setItem('current_user_id', uid);
       localStorage.setItem(`u:${uid}:user`, JSON.stringify(user));
-      window.location.reload();
+      try {
+      const [avail, mine, prefs] = await Promise.all([
+      this.request('/courses/available'),
+      this.request('/courses/my'),
+      this.request('/preferences'),
+    ]);
+
+   
+
+  } catch (e) {
+    console.error('刷新用户数据失败：', e);
+  }
     }
     return result.data;
   }
